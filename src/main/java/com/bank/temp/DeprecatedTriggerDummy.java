dfd1da30a93d33c1d2155026677d7c6376e0bdac
Alessa <alessuhestaras@gmail.com>
2026-04-21 15:53:26 +0800

    added javadoc for the deprecated dummy code

--- src/main/java/com/bank/temp/DeprecatedTriggerDummy.java
+++ src/main/java/com/bank/temp/DeprecatedTriggerDummy.java
@@ -2,6 +2,9 @@ package com.bank.temp;
 
 public class DeprecatedTriggerDummy {
 
+    /**
+     * @deprecated
+     */
     @Deprecated(forRemoval = true)
     public void trigger() {
         Thread thread = new Thread(() -> {
